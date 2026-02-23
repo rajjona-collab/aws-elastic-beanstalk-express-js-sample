c3a274fdf494177db2c3b6994954952b8201ab50
rajjona-collab <rajjona@gmail.com>
2026-02-22 20:58:37 -0600

    second pipeline test by adding line breaks

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is to create a continues delivery pipeline! Pileline has been created using AWS CodePipeline and AWS CodeBuild. Manual review stage has been added to the pipeline between build and deploy stages.'));
+app.get('/', (req, res) => res.send('This is to create a continues delivery pipeline! Pileline has been created using AWS CodePipeline and AWS CodeBuild. <br>Manual review stage has been added to the pipeline between build and deploy stages. <br> second pipeline test has been created to test the changes in the staging environment before deploying to production environment. <br> AWS Elastic Beanstalk is used for deployment.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
