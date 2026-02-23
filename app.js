dc30d77fbcf65b0d3bc09e7bf40f2ded56b066a6
rajjona-collab <rajjona@gmail.com>
2026-02-22 20:48:08 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is to create acontinues delivery pipeline!'));
+app.get('/', (req, res) => res.send('This is to create a continues delivery pipeline! Pileline has been created using AWS CodePipeline and AWS CodeBuild. Manual review stage has been added to the pipeline between build and deploy stages.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
